cf803aa5fe21499dfd058f62e770a13d08446218
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2026-01-25 00:05:55 -0600

    index

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -29,3 +29,36 @@ async function getSpotlights() {
 }
 
 getSpotlights();
+
+const url = "data/members.json";
+
+async function getMembers() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displaySpotlights(data);
+}
+
+getMembers();
+
+function displaySpotlights(members) {
+    const container = document.querySelector("#spotlight-container");
+
+    const filtered = members.filter(
+    member => member.membership === 2 || member.membership === 3
+  );
+
+ const randomMembers = filtered.sort(() => 0.5 - Math.random()).slice(0, 2);
+
+  randomMembers.forEach(member => {
+    const card = document.createElement("div");
+    card.classList.add("spotlight");
+
+    card.innerHTML = `
+      <h3>${member.name}</h3>
+      <img src="images/${member.image}" alt="${member.name}">
+      <p>${member.description}</p>
+      <a href="${member.website}" target="_blank">Visit website</a>
+    `;
+    container.appendChild(card);
+    });
+}  
