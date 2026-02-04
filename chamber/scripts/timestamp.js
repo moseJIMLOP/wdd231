73674729d956f5b3433003256bb8a6f32c4ab388
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2026-02-03 23:28:07 -0600

    join

--- chamber/scripts/timestamp.js
+++ chamber/scripts/timestamp.js
@@ -1,8 +1,8 @@
-<scrip>
+<script>
     document.querySelectorAll('.membership-cards a').forEach(link => {
         link.addEventListener('click', event => {
             event.preventDefault();
             document.querySelector(link.getAttribute('href')).showModal();
         });
     });
-</scrip>    
+</script>
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,26 +1,25 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherDesc = document.querySelector("#weather-desc");
-
 const day1 = document.querySelector("#day1");
 const day2 = document.querySelector("#day2");
 const day3 = document.querySelector("#day3");
 
-const apiKey = "MY_API_KEY";
-const lat = 19.44
+const apiKey = "TU_API_KEY";
+const lat = 19.44;
 const lon = -99.13;
 
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
 async function getWeather() {
     const response = await fetch(url);
     const data = await response.json();
 
-    currentTemp.textContent = Math.round(data.current.temp);
+    currentTemp.textContent = `${Math.round(data.current.temp)}°C`;
     weatherDesc.textContent = data.current.weather[0].description;
-}
 
-day1.textContent = Math.round(data.daily[1].temp.day);
-day2.textContent = Math.round(data.daily[2].temp.day);
-day3.textContent = Math.round(data.daily[3].temp.day);
+    day1.textContent = `${Math.round(data.daily[1].temp.day)}°C`;
+    day2.textContent = `${Math.round(data.daily[2].temp.day)}°C`;
+    day3.textContent = `${Math.round(data.daily[3].temp.day)}°C`;
+}
 
 getWeather();
