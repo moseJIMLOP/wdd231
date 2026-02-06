4abb8411f2e8ef930b243a211c9d0a72ab456a3c
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2026-02-06 11:32:35 -0600

    weather

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -8,18 +8,18 @@ const apiKey = "52d1e79df11cc63a35e6767ce21a6f2e";
 const lat = 19.44;
 const lon = -99.13;
 
-const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
 async function getWeather() {
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(weatherURL);
+  const data = await response.json();
 
-    currentTemp.textContent = `${Math.round(data.current.temp)}°C`;
-    weatherDesc.textContent = data.current.weather[0].description;
+  currentTemp.textContent = `${Math.round(data.list[0].main.temp)}°C`;
+  weatherDesc.textContent = data.list[0].weather[0].description;
 
-    day1.textContent = `${Math.round(data.daily[1].temp.day)}°C`;
-    day2.textContent = `${Math.round(data.daily[2].temp.day)}°C`;
-    day3.textContent = `${Math.round(data.daily[3].temp.day)}°C`;
+  day1.textContent = `${Math.round(data.list[8].main.temp)}°C`;
+  day2.textContent = `${Math.round(data.list[16].main.temp)}°C`;
+  day3.textContent = `${Math.round(data.list[24].main.temp)}°C`;
 }
 
 getWeather();
